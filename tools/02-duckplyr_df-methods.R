fc10698518650d8b503de9cffb6e21f88c1b8cac
Kirill Müller <krlmlr+r@mailbox.org>
2022-12-10 19:55:42 +0100

    Don't need to implement slice_* methods()

--- tools/02-duckplyr_df-methods.R
+++ tools/02-duckplyr_df-methods.R
@@ -16,7 +16,10 @@ df_methods <-
   # special dplyr methods, won't implement
   filter(!(name %in% c("dplyr_col_modify", "dplyr_row_slice"))) %>%
   # methods we don't need to implement but can test
-  filter(!(name %in% c("collapse", "collect", "compute", "count", "tally"))) %>%
+  filter(!(name %in% c(
+    "collapse", "collect", "compute", "count", "tally",
+    "slice_min", "slice_max", "slice_head", "slice_tail", "slice_sample"
+  ))) %>%
   mutate(is_tbl_return = !(name %in% c(
     # Special case: forward to `NextMethod()`, don't change implementation
     "dplyr_reconstruct",
