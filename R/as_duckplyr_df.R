1d81a653ab3d1cbf0c6e21e1d3a374b87fc21a60
Kirill Müller <krlmlr+r@mailbox.org>
2022-12-11 17:58:59 +0100

    Stricter as_duckplyr_df()

--- R/as_duckplyr_df.R
+++ R/as_duckplyr_df.R
@@ -1,6 +1,17 @@
 #' @export
-as_duckplyr_df <- function(.data, ...) {
-  stopifnot(is.data.frame(.data))
+as_duckplyr_df <- function(.data) {
+  if (inherits(.data, "duckplyr_df")) {
+    return(.data)
+  }
+
+  if (!identical(class(.data), "data.frame") && !identical(class(.data), c("tbl_df", "tbl", "data.frame"))) {
+    abort("Must pass a plain data frame or a tibble to `as_duckplyr_df()`.")
+  }
+
+  if (is.character(.row_names_info(.data, 0L))) {
+    abort("Must pass data frame without row names to `as_duckplyr_df()`.")
+  }
+
   class(.data) <- c("duckplyr_df", class(.data))
   .data
 }
