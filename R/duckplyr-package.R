7f888b867392629dbcedb19e345fdc505b35c33c
Kirill Müller <krlmlr+r@mailbox.org>
2022-12-11 08:17:58 +0100

    Import tibble functions and rlang

--- R/duckplyr-package.R
+++ R/duckplyr-package.R
@@ -2,17 +2,19 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import rlang
 #' @importFrom dplyr collapse
 #' @importFrom dplyr collect
 #' @importFrom dplyr compute
 #' @importFrom dplyr count
+#' @importFrom dplyr is_grouped_df
 #' @importFrom dplyr slice_head
 #' @importFrom dplyr slice_max
 #' @importFrom dplyr slice_min
 #' @importFrom dplyr slice_sample
 #' @importFrom dplyr slice_tail
 #' @importFrom dplyr tally
-#' @importFrom rlang as_label
-#' @importFrom rlang enexpr
+#' @importFrom tibble new_tibble
+#' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
