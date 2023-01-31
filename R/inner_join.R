079e7118b1b463ced127cfc87665274811e2c77d
Kirill Müller <krlmlr+r@mailbox.org>
2023-01-31 14:32:37 +0100

    Implement inner_join()

--- R/inner_join.R
+++ R/inner_join.R
@@ -3,6 +3,7 @@
 #' @export
 inner_join.duckplyr_df <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ..., keep = NULL, na_matches = c("na", "never"), multiple = NULL, unmatched = "drop") {
   check_dots_empty0(...)
+  error_call <- caller_env()
 
   x_names <- tbl_vars(x)
   y_names <- tbl_vars(y)
@@ -34,8 +35,40 @@ inner_join.duckplyr_df <- function(x, y, by = NULL, copy = FALSE, suffix = c(".x
     "No implicit cross joins for inner_join()" = cross,
     "No relational implementation for inner_join(copy = TRUE)" = copy,
     {
+      x_by <- by$x
+      y_by <- by$y
       x_rel <- duckdb_rel_from_df(x)
-      y_rel <- duckdb_rel_from_df(x)
+      y_rel <- duckdb_rel_from_df(y)
+
+      # Rename if non-unique column names
+      if (length(intersect(x_names, y_names)) != 0) {
+        x_names <- paste0(x_names, "_x")
+        x_by <- paste0(x_by, "_x")
+        x_exprs <- exprs_from_loc(x, set_names(seq_along(x_names), x_names))
+        x_rel <- rel_project(x_rel, x_exprs)
+
+        y_names <- paste0(y_names, "_y")
+        y_by <- paste0(y_by, "_y")
+        y_exprs <- exprs_from_loc(y, set_names(seq_along(y_names), y_names))
+        y_rel <- rel_project(y_rel, y_exprs)
+      }
+
+      x_by <- map(x_by, relexpr_reference, rel = x_rel)
+      y_by <- map(y_by, relexpr_reference, rel = y_rel)
+
+      conds <- map2(x_by, y_by, ~ relexpr_function(by$condition, list(.x, .y)))
+
+      joined <- rel_join(x_rel, y_rel, conds, "inner")
+
+      exprs <- c(
+        nexprs_from_loc(x_names, vars$x$out),
+        nexprs_from_loc(y_names, vars$y$out)
+      )
+      out <- rel_project(joined, exprs)
+
+      out <- rel_to_df(out)
+      out <- dplyr_reconstruct(out, x)
+
       return(out)
     }
   )
