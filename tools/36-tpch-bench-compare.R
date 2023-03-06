eb90855c9238ff0094d48fb89c2d85ec17e828dd
Kirill Müller <krlmlr+r@mailbox.org>
2023-03-06 22:55:05 +0100

    Filter outlier

--- tools/36-tpch-bench-compare.R
+++ tools/36-tpch-bench-compare.R
@@ -5,5 +5,5 @@ dplyr <- read_csv("res-dplyr.csv")
 
 all <- bind_rows(dplyr, duckdb)
 
-ggplot(all, aes(x = ...1, y = time)) +
-  geom_col()
+ggplot(all, aes(x = ...1, y = time, fill = pkg)) +
+  geom_col(position = "dodge")
