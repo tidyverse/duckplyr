8290cf83e5d6850dbe0bcbbb8e52deab3b8195a0
Kirill Müller <krlmlr+r@mailbox.org>
2023-03-06 20:38:11 +0100

    Up to sf=1

--- tools/31-tpch-load.R
+++ tools/31-tpch-load.R
@@ -5,4 +5,18 @@ lapply(tables, function(t) {
   assign(t, as.data.frame(df), env = .GlobalEnv)
 })
 
-save(list = tables, file = "tools/tpch/001.rda")
+save(list = tables, file = "tools/tpch/001.rda", compress = FALSE)
+
+lapply(tables, function(t) {
+  df <- arrow::read_parquet(fs::path("tools/tpch/010", paste0(t, ".parquet")))
+  assign(t, as.data.frame(df), env = .GlobalEnv)
+})
+
+save(list = tables, file = "tools/tpch/010.rda", compress = FALSE)
+
+lapply(tables, function(t) {
+  df <- arrow::read_parquet(fs::path("tools/tpch/100", paste0(t, ".parquet")))
+  assign(t, as.data.frame(df), env = .GlobalEnv)
+})
+
+save(list = tables, file = "tools/tpch/100.rda", compress = FALSE)
