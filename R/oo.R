56e22b243a5fcc5396f2593591310c257caef8e5
Kirill Müller <krlmlr+r@mailbox.org>
2023-04-09 16:28:33 +0200

    DUCKPLYR_OUTPUT_ORDER

--- R/oo.R
+++ R/oo.R
@@ -1,4 +1,8 @@
 oo_prep <- function(data, colname = "___row_number") {
+  if (Sys.getenv("DUCKPLYR_OUTPUT_ORDER") != "TRUE") {
+    return(data)
+  }
+
   if (colname %in% names(data)) {
     abort("Must use column name not yet present in data")
   }
@@ -7,6 +11,10 @@ oo_prep <- function(data, colname = "___row_number") {
 }
 
 oo_restore <- function(data, colname = "___row_number") {
+  if (Sys.getenv("DUCKPLYR_OUTPUT_ORDER") != "TRUE") {
+    return(data)
+  }
+
   col <- sym(colname)
   sorted <- arrange(data, !!col)
   select(sorted, -!!col)
