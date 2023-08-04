af1efedb25c2d0bf13ca83f0179e4e2fc4d295c0
Kirill Müller <kirill@cynkra.com>
2023-08-04 17:58:45 +0200

    Remove head() implementation

--- tools/90-patch-dplyr.R
+++ tools/90-patch-dplyr.R
@@ -18,7 +18,7 @@ dplyr_texts <- map_chr(dplyr_files, brio::read_file)
 dplyr_texts <- str_replace_all(dplyr_texts, call_rx, "\\1_data_frame\\2")
 walk2(dplyr_texts, dplyr_files, brio::write_file)
 
-forbidden <- fs::path("R", c("dplyr.R", "zzz.R", "duckplyr-package.R"))
+forbidden <- fs::path("R", c("dplyr.R", "zzz.R", "duckplyr-package.R", "head.R"))
 duckplyr_files <- setdiff(fs::dir_ls("R"), forbidden)
 duckplyr_texts <- map_chr(duckplyr_files, brio::read_file)
 duckplyr_texts <- str_replace_all(duckplyr_texts, "dplyr:::([a-z_]+)[.]data[.]frame", "\\1_data_frame")
