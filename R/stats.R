58847bb94dd8a6d6b85c888f100c05ba356b45cb
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-08-28 14:50:34 +0200

    fix: fix my ascii fix :zany_face:

--- R/stats.R
+++ R/stats.R
@@ -4,7 +4,7 @@ stats <- new_environment(list(attempts = 0L, fallback = 0L, calls = character())
 stats_show <- function() {
   writeLines(paste0(
     c("\U0001f6e0", "\U0001f528", "\U0001f986"),
-    paste0(c("\u003A", " ")),
+    paste0("\u003A", " "),
     format(c(stats$attempts, stats$fallback, stats$attempts - stats$fallback))
   ))
   calls <- sort(gsub("[.]duckplyr_df", "", stats$calls))
