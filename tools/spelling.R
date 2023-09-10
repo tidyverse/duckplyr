598427d100aeb1c3c6adc51b7fc8f2c555077b2f
Kirill Müller <kirill@cynkra.com>
2023-09-10 22:30:58 +0200

    chore: Spelling

--- tools/spelling.R
+++ tools/spelling.R
@@ -1,4 +1,4 @@
-words <- sort(c("dplyr", "implementer", "DuckDB", "symdiff"))
+words <- sort(c("dplyr", "implementer", "implementer's", "DuckDB", "symdiff"))
 saveRDS(words, file = ".aspell/duckplyr.rds", version = 2)
 
 
