b06d3a28117c869cdda05934d13ab019a46f5151
Kirill Müller <kirill@cynkra.com>
2023-09-14 13:49:02 -0500

    is_duckplyr_df()

--- R/is_duckplyr_df.R
+++ R/is_duckplyr_df.R
@@ -1,8 +1,20 @@
 #' Class predicate for duckplyr data frames
 #'
-#' @param .data Data
+#' Tests if the input object is of class `"duckplyr_df"`.
+#'
+#' @param .data The object to test
+#'
+#' @return `TRUE` if the input object is of class `"duckplyr_df"`,
+#'   otherwise `FALSE`.
 #'
 #' @export
+#' @examples
+#' tibble(a = 1:3) %>%
+#'   is_duckplyr_df()
+#'
+#' tibble(a = 1:3) %>%
+#'   as_duckplyr_df() %>%
+#'   is_duckplyr_df()
 is_duckplyr_df <- function(.data) {
   inherits(.data, "duckplyr_df")
 }
