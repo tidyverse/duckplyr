f2192be30cb5b5a5f2d8c2155ea95cdb866855f3
Kirill Müller <kirill@cynkra.com>
2024-02-22 11:10:34 +0100

    chore: Better duckdb tests

--- tools/06-patch-duckdb.R
+++ tools/06-patch-duckdb.R
@@ -3,4 +3,5 @@ source("tools/00-funs.R", echo = TRUE)
 brio::read_file("tests/testthat/test-rel_api.R") %>%
   str_replace_all("duckdb(?:::|[$])", "") %>%
   str_replace_all("DBI::", "") %>%
+  str_replace_all("\n[^\n]+asNamespace[^\n]+\n", "\n") %>%
   brio::write_file("../duckdb-r/tests/testthat/test-rel_api.R")
