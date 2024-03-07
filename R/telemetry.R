d4fe04570ec4f458b19710f8b1ad5f0f8dab2700
Kirill Müller <kirill@cynkra.com>
2024-03-07 17:54:50 +0100

    Include scrubbed call

--- R/telemetry.R
+++ R/telemetry.R
@@ -28,7 +28,46 @@ df_to_json <- function(df, known_names = NULL) {
 arg_to_json <- function(x, known_names) {
   if (is.atomic(x)) {
     x
+  } else if (is_quosures(x)) {
+    quos_to_json(x, known_names)
+  } else if (is_quosure(x)) {
+    quo_to_json(x, known_names)
+  } else if (is_call(x)) {
+    expr_to_json(x, known_names)
   } else {
     paste0("Can't translate object of class ", paste(class(x), collapse = "/"))
   }
+}
+
+quos_to_json <- function(x, known_names) {
+  map(x, ~ quo_to_json(.x, known_names))
+}
+
+quo_to_json <- function(x, known_names) {
+  expr_to_json(quo_get_expr(x), known_names)
+}
+
+expr_to_json <- function(x, known_names) {
+  scrubbed <- expr_scrub(x, known_names)
+  expr_deparse(scrubbed, width = 500L)
+}
+
+expr_scrub <- function(x, known_names) {
+  do_scrub <- function(xx) {
+    if (is_call(xx)) {
+      args <- map(as.list(xx)[-1], do_scrub)
+      call2(xx[[1]], !!!args)
+    } else if (is_symbol(xx)) {
+      match <- match(as.character(xx), known_names)
+      if (is.na(match)) {
+        xx
+      } else {
+        sym(paste0("...", match))
+      }
+    } else {
+      paste0("Don't know how to scrub ", paste(class(xx), collapse = "/"))
+    }
+  }
+
+  do_scrub(x)
 }
