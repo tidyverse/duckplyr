7b323d58e194570e42f3d5dabfbdb01704672ced
Kirill Müller <kirill@cynkra.com>
2024-03-08 10:29:10 +0100

    10

--- R/relational-duckdb.R
+++ R/relational-duckdb.R
@@ -113,7 +113,7 @@ check_df_for_rel <- function(df) {
       cli_abort("Can't convert named vectors to relational. Affected column: {.var {names(df)[[i]]}}.")
     }
     if (!is.null(dim(col))) {
-      stop("Can't convert arrays or matrices to relational. Affected column: `", names(df)[[i]], "`.")
+      cli_abort("Can't convert arrays or matrices to relational. Affected column: {.var {names(df)[[i]]}}.")
     }
     if (isS4(col)) {
       stop("Can't convert S4 columns to relational. Affected column: `", names(df)[[i]], "`.")
