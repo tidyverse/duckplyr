c2bffbe4b2c91eda8a688e5758cbe5c6a2604c12
Kirill Müller <kirill@cynkra.com>
2024-03-08 12:50:45 +0100

    Skips

--- tests/testthat/test-group-map.R
+++ tests/testthat/test-group-map.R
@@ -24,6 +24,7 @@ test_that("duckplyr_group_map() can return arbitrary objects", {
 })
 
 test_that("duckplyr_group_map() works on ungrouped data frames (#4067)", {
+  skip("TODO duckdb")
   expect_identical(
     duckplyr_group_map(mtcars, ~ head(.x, 2L)),
     list(head(as_tibble(mtcars), 2L))
