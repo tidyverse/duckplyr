d46d031154dd191e19c190a23b1d2f1ac1c653ea
Kirill Müller <kirill@cynkra.com>
2024-03-17 17:19:25 +0100

    New df_from_csv() and duckdb_df_from_csv()

--- R/io.R
+++ R/io.R
@@ -1,9 +1,6 @@
 #' Read Parquet, CSV, and other files using DuckDB
 #'
-#' This function ingests data from files.
-#' Internally, a DuckDB table-valued function is called,
-#' the results are transparently converted to a data frame.
-#' The data is only read when the data is actually accessed.
+#' `df_from_file()` uses arbitrary table functions to read data.
 #' See <https://duckdb.org/docs/data/overview> for a documentation
 #' of the available functions and their options.
 #'
@@ -21,27 +18,6 @@
 #'   `duckplyr_df_from_file()`, extended by the provided `class`.
 #'
 #' @export
-#' @examples
-#' # Create simple CSV file
-#' path <- tempfile(fileext = ".csv")
-#' write.csv(data.frame(a = 1:3, b = letters[4:6]), path, row.names = FALSE)
-#'
-#' # Reading is immediate
-#' df <- df_from_file(path, "read_csv_auto")
-#'
-#' # Materialization only upon access
-#' names(df)
-#' df$a
-#'
-#' # Return as tibble:
-#' df_from_file(
-#'   path,
-#'   "read_csv",
-#'   options = list(delim = ",", auto_detect = TRUE),
-#'   class = class(tibble())
-#' )
-#'
-#' unlink(path)
 df_from_file <- function(path,
                          table_function,
                          options = list(),
