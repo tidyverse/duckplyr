2979f815a28ae4b07971e41d986eda63be39f98b
apalacio9502 <alejandro.palacio@outlook.com>
2024-06-02 20:27:06 -0500

    mutate() now supports the argument .keep = "transmute"

--- R/dplyr-reimplement.R
+++ R/dplyr-reimplement.R
@@ -5,14 +5,19 @@ duckplyr_mutate_keep <- function(out, keep, used, names_new, names_groups) {
 
   names <- names(out)
 
-  names_keep <- switch(keep,
-    used = names(used)[used],
-    unused = names(used)[!used],
-    none = character(),
-    abort("Unknown `keep`.", .internal = TRUE)
-  )
-
-  names_out <- intersect(names, c(names_new, names_groups, names_keep))
+  if (keep == "transmute") {
+    names_groups <- setdiff(names_groups, names_new)
+    names_out <- c(names_groups, names_new)
+  } else {
+    names_keep <- switch(
+      keep,
+      used = names(used)[used],
+      unused = names(used)[!used],
+      none = character(),
+      abort("Unknown `keep`.", .internal = TRUE)
+    )
+    names_out <- intersect(names, c(names_new, names_groups, names_keep))
+  }
 
   select(out, !!!names_out)
 }
