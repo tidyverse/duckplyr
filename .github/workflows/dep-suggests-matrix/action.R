4692dc8d7fe24f6985570a2186d3dab41e39a971
Kirill Müller <kirill@cynkra.com>
2024-08-20 04:58:55 +0200

    ..

--- .github/workflows/dep-suggests-matrix/action.R
+++ .github/workflows/dep-suggests-matrix/action.R
@@ -22,9 +22,11 @@ get_deps <- function() {
 if (Sys.getenv("GITHUB_BASE_REF") != "") {
   print(Sys.getenv("GITHUB_BASE_REF"))
   system("git fetch origin ${GITHUB_BASE_REF}")
-  has_diff <- (system("git diff origin/${GITHUB_BASE_REF}... | egrep '^[+][^+]' | grep -q ::") == 0)
+  # Use .. to avoid having to fetch the entire history
+  # https://github.com/krlmlr/actions-sync/issues/45
+  has_diff <- (system("git diff origin/${GITHUB_BASE_REF}.. | egrep '^[+][^+]' | grep -q ::") == 0)
   if (has_diff) {
-    system("git diff origin/${GITHUB_BASE_REF}... | egrep '^[+][^+]' | grep -q ::")
+    system("git diff origin/${GITHUB_BASE_REF}.. | egrep '^[+][^+]' | grep -q ::")
     packages <- get_deps()
   } else {
     writeLines("No changes using :: found, not checking without suggested packages")
