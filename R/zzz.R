7c5dfaf6be0da7fd6f41ad73b8986137484feb18
Kirill Müller <kirill@cynkra.com>
2024-10-19 16:08:05 +0200

    Suppress

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,10 @@
 .onAttach <- function(lib, pkg) {
   if (!exists(".__DEVTOOLS__", asNamespace("duckplyr"))) {
     msg <- character()
-    try_fetch(methods_overwrite(), message = function(cond) {
+    suppressMessages(try_fetch(methods_overwrite(), message = function(cond) {
       msg <<- c(msg, conditionMessage(cond))
       zap()
-    })
+    }))
     packageStartupMessage(msg)
   }
 }
@@ -16,10 +16,10 @@
 .onDetach <- function(lib) {
   if (!exists(".__DEVTOOLS__", asNamespace("duckplyr"))) {
     msg <- character()
-    try_fetch(methods_restore(), message = function(cond) {
+    suppressMessages(try_fetch(methods_restore(), message = function(cond) {
       msg <<- c(msg, conditionMessage(cond))
       zap()
-    })
+    }))
     packageStartupMessage(msg)
   }
 }
