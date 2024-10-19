c81ae2a12bd72bc7d09f69aac0a7b043600c6ffa
Kirill Müller <kirill@cynkra.com>
2024-10-19 19:22:53 +0200

    if()

--- R/anti_join.R
+++ R/anti_join.R
@@ -9,7 +9,7 @@ anti_join.duckplyr_df <- function(x, y, by = NULL, copy = FALSE, ..., na_matches
   na_matches <- check_na_matches(na_matches, error_call = error_call)
 
   # Our implementation
-  rel_try(list(name = "anti_join", x = x, y = y, args = list(by = if(!is.null(by) && !is_cross_by(by)) as_join_by(by), copy = copy, na_matches = na_matches)),
+  rel_try(list(name = "anti_join", x = x, y = y, args = list(by = if (!is.null(by) && !is_cross_by(by)) as_join_by(by), copy = copy, na_matches = na_matches)),
     {
       out <- rel_join_impl(x, y, by, "anti", na_matches, error_call = error_call)
       return(out)
