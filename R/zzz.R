f629a75c28e64dc3588eee584f0a6e854690960f
Kirill Müller <kirill@cynkra.com>
2024-10-19 09:35:05 +0200

    Use try_fetch()

--- R/zzz.R
+++ R/zzz.R
@@ -4,14 +4,22 @@
 
 .onAttach <- function(lib, pkg) {
   if (!exists(".__DEVTOOLS__", asNamespace("duckplyr"))) {
-    msg <- withCallingHandlers(methods_overwrite(), message = conditionMessage)
+    msg <- character()
+    try_fetch(methods_overwrite(), message = function(cond) {
+      msg <<- c(msg, conditionMessage(cond))
+      zap()
+    })
     packageStartupMessage(msg)
   }
 }
 
 .onDetach <- function(lib) {
   if (!exists(".__DEVTOOLS__", asNamespace("duckplyr"))) {
-    msg <- withCallingHandlers(methods_restore(), message = conditionMessage)
+    msg <- character()
+    try_fetch(methods_restore(), message = function(cond) {
+      msg <<- c(msg, conditionMessage(cond))
+      zap()
+    })
     packageStartupMessage(msg)
   }
 }
