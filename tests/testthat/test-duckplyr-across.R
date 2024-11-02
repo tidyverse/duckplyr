9743558f717432fa12f17f2c27543341d3bb484c
Kirill Müller <kirill@cynkra.com>
2024-11-02 14:15:28 +0100

    Add tests

---
+++ tests/testthat/test-duckplyr-across.R
@@ -0,0 +1,80 @@
+test_that("duckplyr_expand_across() successful", {
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y"),
+      across(x:y, mean)
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y"),
+      across(x:y, function(x) mean(x))
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y"),
+      across(c(x_mean = x, y_mean = y), mean)
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y"),
+      across(c(x_mean = x, y_mean = y), mean, .names = "{.col}_{.fn}")
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y"),
+      across(x:y, function(x) mean(x, na.rm = TRUE))
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y", "a"),
+      across(c(a, x), function(x) x + 1)
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y", "a"),
+      across(c(a, x), function(x) x * 2 + 1)
+    )
+  })
+
+  expect_snapshot({
+    test_duckplyr_expand_across(
+      c("x", "y", "a"),
+      across(-a, function(x) x * x)
+    )
+  })
+})
+
+test_that("duckplyr_expand_across() failing", {
+  expect_null(test_duckplyr_expand_across(
+    c("x", "y"),
+    across(x:y, mean, .unpack = TRUE)
+  ))
+  expect_null(test_duckplyr_expand_across(
+    c("x", "y"),
+    across(x:y, mean, na.rm = TRUE)
+  ))
+  expect_null(test_duckplyr_expand_across(
+    c("x", "y"),
+    across(x:y, list(mean))
+  ))
+  expect_null(test_duckplyr_expand_across(
+    c("x", "y"),
+    across(x:y, list(mean = mean))
+  ))
+  expect_null(test_duckplyr_expand_across(
+    c("x", "y"),
+    across(x:y, list(mean = mean, median = median))
+  ))
+})
