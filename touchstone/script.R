b8ecdd9ffab5fef5c53e6cc3679912d6e3b46f39
Kirill Müller <kirill@cynkra.com>
2024-11-04 05:37:19 +0100

    Prune

--- touchstone/script.R
+++ touchstone/script.R
@@ -2,16 +2,6 @@ library(touchstone)
 
 branch_install()
 
-clear_branch_caches <- function() {
-  purrr::walk(c(branch_get_or_fail("GITHUB_BASE_REF"), branch_get_or_fail("GITHUB_HEAD_REF")),
-    styler::cache_clear,
-    ask = FALSE
-  )
-}
-
-clear_branch_caches()
-
-
 benchmark_run(
   expr_before_benchmark = {
     library(styler)
