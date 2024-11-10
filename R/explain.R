8005e5d636340cf01e8ca9d8664d3580896742e1
Kirill Müller <krlmlr@users.noreply.github.com>
2024-11-10 11:35:15 +0100

    chore: `explain()` returns the input, invisibly (#331)

--- R/explain.R
+++ R/explain.R
@@ -5,10 +5,22 @@ explain.duckplyr_df <- function(x, ...) {
     {
       rel <- duckdb_rel_from_df(x)
       rel_explain(rel)
-      return(invisible())
+      return(invisible(x))
     }
   )
 
   writeLines("Can't convert to relational, fallback implementation will be used.")
-  invisible()
+  invisible(x)
+}
+
+duckplyr_explain <- function(.data, ...) {
+  try_fetch(
+    .data <- as_duckplyr_df(.data),
+    error = function(e) {
+      testthat::skip(conditionMessage(e))
+    }
+  )
+  out <- explain(.data, ...)
+  class(out) <- setdiff(class(out), "duckplyr_df")
+  invisible(out)
 }
