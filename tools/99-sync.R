4ccb747ad8deef4c660e861fdc8211f3b2455f27
Kirill Müller <krlmlr@users.noreply.github.com>
2024-11-21 14:12:53 +0100

    docs: Include section on code generation in contributing guide (#348)

--- tools/99-sync.R
+++ tools/99-sync.R
@@ -1,3 +1,12 @@
+# Once per clone:
+
+# - source("tools/30-tpch-export.R", echo = TRUE)
+# - source("tools/31-tpch-load-qs.R", echo = TRUE)
+# - Follow instructions in .sync/README.md
+# - Restart R
+
+# Every time you want to sync:
+
 source("tools/01-dplyr-methods.R", echo = TRUE)
 source("tools/02-duckplyr_df-methods.R", echo = TRUE)
 source("tools/03-tests.R", echo = TRUE)
@@ -5,7 +14,5 @@ source("tools/04-dplyr-tests.R", echo = TRUE)
 source("tools/05-duckdb-tests.R", echo = TRUE)
 source("tools/06-patch-duckdb.R", echo = TRUE)
 source("tools/07-overwrite.R", echo = TRUE)
-#source("tools/30-tpch-export.R", echo = TRUE)
-#source("tools/31-tpch-load-qs.R", echo = TRUE)
 source("tools/37-tpch-peel.R", echo = TRUE)
 source("tools/39-tpch-peel-oo.R", echo = TRUE)
