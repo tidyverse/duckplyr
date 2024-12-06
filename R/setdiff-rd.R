436242110611029a7f8cf1fa80dd61bb8e69f869
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-12-06 10:24:07 +0100

    docs: add manual page for `setdiff.duckplyr_df()`

---
+++ R/setdiff-rd.R
@@ -0,0 +1,16 @@
+#' @title Set diff
+#'
+#' @description  This is a method for the [`dplyr::setdiff()`] generic.
+#' See "Fallbacks" section for differences in implementation.
+#' `setdiff(x, y)` finds all rows in `x` that aren't in `y`.
+#'
+#' @inheritParams dplyr::setdiff
+#' @examples
+#' df1 <- tibble(x = 1:3)
+#' df2 <- tibble(x = 3:5)
+#' setdiff(df1, df2)
+#' setdiff(df2, df1)
+#' @seealso [`dplyr::setdiff()`]
+#' @rdname setdiff.duckplyr_df
+#' @name setdiff.duckplyr_df
+NULL
