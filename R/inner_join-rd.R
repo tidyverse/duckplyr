97f2e0a3f63ebcc753585c66575d600533122da8
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-12-13 19:57:07 +0100

    docs: Add manual pages for dplyr methods (#359)

---
+++ R/inner_join-rd.R
@@ -0,0 +1,15 @@
+#' @title Inner join
+#'
+#' @description  This is a method for the [`dplyr::inner_join()`] generic.
+#' See "Fallbacks" section for differences in implementation.
+#' An `inner_join()` only keeps observations from `x`
+#' that have a matching key in `y`.
+#'
+#' @inheritParams dplyr::inner_join
+#' @examples
+#' library("dplyr")
+#' inner_join(band_members, band_instruments)
+#' @seealso [`dplyr::inner_join()`]
+#' @rdname inner_join.duckplyr_df
+#' @name inner_join.duckplyr_df
+NULL
---
+++ R/union_all-rd.R
@@ -0,0 +1,15 @@
+#' @title Union of all
+#'
+#' @description  This is a method for the [`dplyr::union_all()`] generic.
+#' See "Fallbacks" section for differences in implementation.
+#' `union_all(x, y)` finds all rows in either x or y, including duplicates.
+#'
+#' @inheritParams dplyr::union_all
+#' @examples
+#' df1 <- tibble(x = 1:3)
+#' df2 <- tibble(x = 3:5)
+#' union_all(df1, df2)
+#' @seealso [`dplyr::union_all()`]
+#' @rdname union_all.duckplyr_df
+#' @name union_all.duckplyr_df
+NULL
