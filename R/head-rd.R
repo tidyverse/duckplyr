f7997dc83c656d49a1eeb2a9198c0ebf26c853dc
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-12-13 08:48:13 +0100

    docs: add manual page for `head.duckplyr_df()`

---
+++ R/head-rd.R
@@ -0,0 +1,15 @@
+#' @title Return the First Parts of an Object
+#'
+#' @description  This is a method for the [`head()`] generic.
+#' See "Fallbacks" section for differences in implementation.
+#' Return the first rows of a data.frame
+#'
+#' @param x A data.frame
+#' @param n A positive integer, how many rows to return.
+#' @param ... Not used yet.
+#' @examples
+#' head(mtcars, 2)
+#' @seealso [`head()`]
+#' @rdname head.duckplyr_df
+#' @name head.duckplyr_df
+NULL
