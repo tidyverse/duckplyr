cc277ed5944be943f7fea0f884b7e8fcb14cee57
Kirill Müller <kirill@cynkra.com>
2024-12-14 14:39:11 +0100

    Move test

--- tests/testthat/test-as_duckplyr_tibble.R
+++ tests/testthat/test-as_duckplyr_tibble.R
@@ -5,3 +5,12 @@ test_that("as_duckplyr_tibble() works", {
   expect_s3_class(as_duckplyr_tibble(data.frame(a = 1)), "duckplyr_df")
   expect_equal(class(as_duckplyr_tibble(data.frame(a = 1))), c("duckplyr_df", class(tibble())))
 })
+
+test_that("as_duckplyr_df() and special df", {
+  # https://github.com/tidyverse/duckplyr/issues/127
+
+  by_cyl <- dplyr::group_by(mtcars, cyl)
+  expect_snapshot(error = TRUE, {
+    as_duckplyr_df(by_cyl)
+  })
+})
