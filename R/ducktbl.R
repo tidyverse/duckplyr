8fd88c60a0f986012c5c06d4767cbb97b80c7cc8
Kirill Müller <kirill@cynkra.com>
2024-12-16 04:40:33 +0100

    Coerce and use

--- R/ducktbl.R
+++ R/ducktbl.R
@@ -43,14 +43,7 @@
 #' @export
 ducktbl <- function(..., .lazy = FALSE) {
   out <- tibble::tibble(...)
-
-  out <- as_duckplyr_df_impl(out)
-
-  if (.lazy) {
-    out <- as_lazy_duckplyr_df(out)
-  }
-
-  out
+  as_ducktbl(out, .lazy = .lazy)
 }
 
 #' as_ducktbl
@@ -88,11 +81,21 @@ as_ducktbl.tbl_duckdb_connection <- function(x, ...) {
 }
 
 #' @export
+as_ducktbl.data.frame <- function(x, ...) {
+  check_dots_empty()
+
+  # - as_tibble() to remove row names
+  new_ducktbl(as_tibble(x))
+}
+
+#' @export
 as_ducktbl.default <- function(x, ...) {
   check_dots_empty()
 
-  # Extra as.data.frame() call for good measure and perhaps https://github.com/tidyverse/tibble/issues/1556
-  as_duckplyr_df_impl(as_tibble(as.data.frame(x)))
+  # - as.data.frame() call for good measure and perhaps https://github.com/tidyverse/tibble/issues/1556
+  # - as_tibble() to remove row names
+  # Could call as_ducktbl(as.data.frame(x)) here, but that would be slower
+  new_ducktbl(as_tibble(as.data.frame(x)))
 }
 
 #' @export
