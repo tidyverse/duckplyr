d28bd8f1361e57e1532536f72530a23bda2fa071
Kirill Müller <kirill@cynkra.com>
2025-02-21 21:11:39 +0100

    docs: Separate `?compute_parquet` and `?compute_csv`

---
+++ R/compute_parquet.R
@@ -0,0 +1,49 @@
+#' Compute results to a Parquet file
+#'
+#' For a duckplyr frame, this function executes the query
+#' and stores the results in a Parquet file,
+#' without converting it to an R data frame.
+#' The result is a duckplyr frame that can be used with subsequent dplyr verbs.
+#' This function can also be used as a Parquet writer for regular data frames.
+#'
+#' @inheritParams rlang::args_dots_empty
+#' @inheritParams compute.duckplyr_df
+#' @param x A duckplyr frame.
+#' @param path The path of the Parquet file to create.
+#' @param options A list of additional options to pass to create the Parquet file,
+#'   see <https://duckdb.org/docs/data/parquet/overview#writing-to-parquet-files>
+#'   for details.
+#'
+#' @return A duckplyr frame.
+#'
+#' @export
+#' @examples
+#' library(duckplyr)
+#' df <- data.frame(x = c(1, 2))
+#' df <- mutate(df, y = 2)
+#' path <- tempfile(fileext = ".parquet")
+#' df <- compute_parquet(df, path)
+#' explain(df)
+#' @seealso [compute_csv()], [compute.duckplyr_df()], [dplyr::collect()]
+compute_parquet <- function(x, path, ..., prudence = NULL, options = NULL) {
+  check_dots_empty()
+
+  if (is.null(options)) {
+    options <- list()
+  }
+
+  if (is.null(prudence)) {
+    prudence <- get_prudence_duckplyr_df(x)
+  }
+
+  rel <- duckdb_rel_from_df(x)
+
+  duckdb$rel_to_parquet(rel, path, options)
+
+  # If the path is a directory, we assume that the user wants to write multiple files
+  if (dir.exists(path)) {
+    path <- file.path(path, "**", "**.parquet")
+  }
+
+  read_parquet_duckdb(path, prudence = prudence)
+}
--- tests/testthat/test-compute_parquet.R
+++ tests/testthat/test-compute_parquet.R
@@ -16,21 +16,3 @@ test_that("compute_parquet() with options", {
   expect_identical(out, as_duckdb_tibble(df))
   expect_false(is_prudent_duckplyr_df(out))
 })
-
-test_that("compute_csv()", {
-  df <- data.frame(x = c(1, 2))
-  withr::defer(unlink("test.csv"))
-  out <- compute_csv(df, path = "test.csv")
-
-  expect_identical(out, as_duckdb_tibble(df))
-  expect_false(is_prudent_duckplyr_df(out))
-})
-
-test_that("compute_csv() prudence", {
-  df <- data.frame(x = c(1, 2))
-  withr::defer(unlink("test.csv"))
-  out <- compute_csv(df, path = "test.csv", prudence = "stingy")
-
-  expect_true(is_prudent_duckplyr_df(out))
-  expect_identical(collect(out), as_tibble(df))
-})
