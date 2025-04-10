a4887caef9a045e1dc2843c7b3c5fbbe4a9130c6
Kirill Müller <kirill@cynkra.com>
2025-04-10 07:05:19 +0200

    feat: New `as_tbl()` to convert to a dbplyr tbl object

---
+++ R/as_tbl.R
@@ -0,0 +1,51 @@
+#' Convert a duckplyr frame to a dbplyr table
+#'
+#' This function converts a lazy duckplyr frame or a data frame
+#' to a dbplyr table in duckplyr's internal connection.
+#' This allows using dbplyr functions on the data,
+#' including hand-written SQL queries.
+#' Use [as_duckdb_tibble()] to convert back to a lazy duckplyr frame.
+#'
+#' @param .data A lazy duckplyr frame or a data frame.
+#' @return A dbplyr table.
+#' @export
+#' @examples
+#' df <- duckdb_tibble(a = 1L)
+#' df
+#'
+#' tbl <- as_tbl(df)
+#' tbl
+#'
+#' tbl %>%
+#'   mutate(b = sql("a + 1")) %>%
+#'   as_duckdb_tibble()
+as_tbl <- function(.data) {
+  check_installed("dbplyr")
+
+  rel <- duckdb_rel_from_df(.data)
+
+  con <- get_default_duckdb_connection()
+  name <- unique_table_name("as_tbl_")
+
+  # Drop view when object goes out of scope
+  scope_guard <- new_environment()
+  reg.finalizer(
+    scope_guard,
+    function(e) {
+      tryCatch(DBI::dbExecute(con, paste0("DROP VIEW ", name)),
+        # Ignore errors
+        error = function(e) {
+          message(conditionMessage(e))
+        }
+      )
+    },
+    onexit = FALSE
+  )
+
+  # Side effect
+  duckdb$rel_to_view(rel, "", name, temporary = TRUE)
+
+  out <- dplyr::tbl(con, name)
+  attr(out, "duckplyr_scope_guard") <- scope_guard
+  out
+}
---
+++ tests/testthat/test-as_tbl.R
@@ -0,0 +1,30 @@
+test_that("as_tbl()", {
+  skip_if_not_installed("dbplyr")
+
+  df <- duckdb_tibble(a = 1L)
+
+  tbl <- as_tbl(df)
+  expect_s3_class(tbl, "tbl_dbi")
+  expect_equal(collect(tbl), tibble(a = 1L))
+
+  sql <- dbplyr::sql_render(tbl)
+
+  out <-
+    tbl %>%
+    mutate(b = sql("a + 1")) %>%
+    as_duckdb_tibble()
+
+  expect_s3_class(out, "duckplyr_df")
+  expect_equal(head(out), duckdb_tibble(a = 1L, b = 2L))
+
+  # Test destruction of underlying view
+  expect_equal(DBI::dbGetQuery(get_default_duckdb_connection(), sql), data.frame(a = 1L))
+
+  rm(tbl)
+  gc()
+
+  expect_error(DBI::dbGetQuery(get_default_duckdb_connection(), sql))
+  # This is expected but perhaps undesirable.
+  # We could carry over the duckplyr_scope_guard to the relational object.
+  expect_error(collect(out))
+})
