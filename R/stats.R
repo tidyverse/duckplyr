ee9a633c79364ee9dea3171f915510597af650a7
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:47:54 +0200

    M  R/stats.R

--- R/stats.R
+++ R/stats.R
@@ -9,14 +9,6 @@ stats <- new_environment(list(attempts = 0L, fallback = 0L, calls = character())
 #' @return Called for its side effect.
 #'
 #' @export
-#' @examples
-#' stats_show()
-#'
-#' tibble(a = 1:3) %>%
-#'   as_duckplyr_tibble() %>%
-#'   mutate(b = a + 1)
-#'
-#' stats_show()
 stats_show <- function() {
   writeLines(paste0(
     c("\U0001f6e0", "\U0001f528", "\U0001f986"),
