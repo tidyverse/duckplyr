17428dc5372ab907e3259f0a48232acc9dd0c319
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:45:14 +0200

    M  R/distinct-rd.R

--- R/distinct-rd.R
+++ R/distinct-rd.R
@@ -5,13 +5,6 @@
 #' This is similar to `unique.data.frame()` but considerably faster.
 #'
 #' @inheritParams dplyr::distinct
-#' @examples
-#' df <- duckdb_tibble(
-#'   x = sample(10, 100, rep = TRUE),
-#'   y = sample(10, 100, rep = TRUE)
-#' )
-#' nrow(df)
-#' nrow(distinct(df))
 #' @seealso [dplyr::distinct()]
 #' @rdname distinct.duckplyr_df
 #' @name distinct.duckplyr_df
