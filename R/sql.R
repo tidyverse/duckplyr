ae4dba355bd59f25350e997c30132d6e85bbb5cb
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:47:50 +0200

    M  R/sql.R

--- R/sql.R
+++ R/sql.R
@@ -16,8 +16,6 @@
 #' @param con The connection, defaults to the default connection.
 #'
 #' @export
-#' @examples
-#' read_sql_duckdb("FROM duckdb_settings()")
 read_sql_duckdb <- function(sql, ..., prudence = c("thrifty", "lavish", "stingy"), con = NULL) {
   if (!is_string(sql)) {
     cli::cli_abort("{.arg sql} must be a string.")
