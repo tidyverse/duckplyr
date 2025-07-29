aab2356f6b585da591d7be307f2e168ef4bec788
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:44:31 +0200

    M  R/anti_join-rd.R

--- R/anti_join-rd.R
+++ R/anti_join-rd.R
@@ -4,9 +4,6 @@
 #' `anti_join()` returns all rows from `x` with**out** a match in `y`.
 #'
 #' @inheritParams dplyr::anti_join
-#' @examples
-#' library(duckplyr)
-#' band_members %>% anti_join(band_instruments)
 #' @seealso [dplyr::anti_join()]
 #' @rdname anti_join.duckplyr_df
 #' @name anti_join.duckplyr_df
