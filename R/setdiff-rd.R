febe288e15f0fb1c67acdeb140393fc2ce44fc01
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:47:42 +0200

    M  R/setdiff-rd.R

--- R/setdiff-rd.R
+++ R/setdiff-rd.R
@@ -5,11 +5,6 @@
 #' `setdiff(x, y)` finds all rows in `x` that aren't in `y`.
 #'
 #' @inheritParams dplyr::setdiff
-#' @examples
-#' df1 <- duckdb_tibble(x = 1:3)
-#' df2 <- duckdb_tibble(x = 3:5)
-#' setdiff(df1, df2)
-#' setdiff(df2, df1)
 #' @seealso [dplyr::setdiff()]
 #' @rdname setdiff.duckplyr_df
 #' @name setdiff.duckplyr_df
