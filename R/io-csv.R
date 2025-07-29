25ac427a2396c829347dd420691f9415f1cce2ce
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:46:04 +0200

    MM R/io-csv.R

--- R/io-csv.R
+++ R/io-csv.R
@@ -9,25 +9,6 @@
 #'
 #' @rdname df_from_file
 #' @export
-#' @examples
-#' # Create simple CSV file
-#' path <- tempfile("duckplyr_test_", fileext = ".csv")
-#' write.csv(data.frame(a = 1:3, b = letters[4:6]), path, row.names = FALSE)
-#'
-#' # Reading is immediate
-#' df <- df_from_csv(path)
-#'
-#' # Materialization only upon access
-#' names(df)
-#' df$a
-#'
-#' # Return as tibble, specify column types:
-#' df_from_file(
-#'   path,
-#'   "read_csv",
-#'   options = list(delim = ",", types = list(c("DOUBLE", "VARCHAR"))),
-#'   class = class(tibble())
-#' )
 df_from_csv <- function(path, ..., options = list(), class = NULL) {
   check_dots_empty()
   lifecycle::deprecate_soft("1.0.0", "df_from_csv()", "read_csv_duckdb()")
