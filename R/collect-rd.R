ccdcd1dc8a643a3830b28691edc089dab958ea4e
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:44:48 +0200

    M  R/collect-rd.R

--- R/collect-rd.R
+++ R/collect-rd.R
@@ -4,13 +4,6 @@
 #' `collect()` converts the input to a tibble, materializing any lazy operations.
 #'
 #' @inheritParams dplyr::collect
-#' @examples
-#' library(duckplyr)
-#' df <- duckdb_tibble(x = c(1, 2), .lazy = TRUE)
-#' df
-#' try(print(df$x))
-#' df <- collect(df)
-#' df
 #' @seealso [dplyr::collect()]
 #' @rdname collect.duckplyr_df
 #' @name collect.duckplyr_df
