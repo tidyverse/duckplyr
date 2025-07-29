42bd1fd89e077c18ac74c5f1f546c77e9a2002e9
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:47:46 +0200

    M  R/slice_head-rd.R

--- R/slice_head-rd.R
+++ R/slice_head-rd.R
@@ -4,11 +4,6 @@
 #' `slice_head()` selects the first rows.
 #'
 #' @inheritParams dplyr::slice_head
-#' @examples
-#' library(duckplyr)
-#' df <- data.frame(x = 1:3)
-#' df <- slice_head(df, n = 2)
-#' df
 #' @seealso [dplyr::slice_head()]
 #' @rdname slice_head.duckplyr_df
 #' @name slice_head.duckplyr_df
