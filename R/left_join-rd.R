3772104c1adca165c34657ac1fb5e6a91f491276
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:46:20 +0200

    M  R/left_join-rd.R

--- R/left_join-rd.R
+++ R/left_join-rd.R
@@ -5,9 +5,6 @@
 #' A `left_join()` keeps all observations in `x`.
 #'
 #' @inheritParams dplyr::left_join
-#' @examples
-#' library(duckplyr)
-#' left_join(band_members, band_instruments)
 #' @seealso [dplyr::left_join()]
 #' @rdname left_join.duckplyr_df
 #' @name left_join.duckplyr_df
