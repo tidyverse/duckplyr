3a3931ccc37b319b8ddba16c114824d8d1bc0552
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:46:37 +0200

    M  R/read_json_duckdb.R

--- R/read_json_duckdb.R
+++ R/read_json_duckdb.R
@@ -9,16 +9,6 @@
 #' @seealso [read_csv_duckdb()], [read_parquet_duckdb()]
 #'
 #' @export
-#' @examplesIf identical(Sys.getenv("IN_PKGDOWN"), "TRUE")
-#'
-#' # Create and read a simple JSON file
-#' path <- tempfile("duckplyr_test_", fileext = ".json")
-#' writeLines('[{"a": 1, "b": "x"}, {"a": 2, "b": "y"}]', path)
-#'
-#' # Reading needs the json extension
-#' db_exec("INSTALL json")
-#' db_exec("LOAD json")
-#' read_json_duckdb(path)
 read_json_duckdb <- function(path, ..., prudence = c("thrifty", "lavish", "stingy"), options = list()) {
   check_dots_empty()
 
