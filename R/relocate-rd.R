417e9f4024ff651c0ce30192fc5d5758a47ca4c0
Kirill Müller <kirill@cynkra.com>
2025-07-29 21:47:22 +0200

    M  R/relocate-rd.R

--- R/relocate-rd.R
+++ R/relocate-rd.R
@@ -6,9 +6,6 @@
 #' using the same syntax as `select()` to make it easy to move blocks of columns at once.
 #'
 #' @inheritParams dplyr::relocate
-#' @examples
-#' df <- duckdb_tibble(a = 1, b = 1, c = 1, d = "a", e = "a", f = "a")
-#' relocate(df, f)
 #' @seealso [dplyr::relocate()]
 #' @rdname relocate.duckplyr_df
 #' @name relocate.duckplyr_df
