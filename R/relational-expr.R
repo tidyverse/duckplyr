a50c3bfa17ba7dd7e56aa18df9f0aa52c7ee6c72
Kirill Müller <kirill@cynkra.com>
2026-02-24 14:49:58 +0100

    Back-compat

--- R/relational-expr.R
+++ R/relational-expr.R
@@ -70,7 +70,7 @@ relexpr_constant <- function(val, alias = NULL) {
 #' @rdname new_relexpr
 #' @return an object of class `"relational_relexpr"`
 #' @export
-relexpr_function <- function(name, args, order_bys = NULL, alias = NULL) {
+relexpr_function <- function(name, args, alias = NULL, order_bys = NULL) {
   stopifnot(is_string(name))
   stopifnot(is.list(args))
   stopifnot(is.null(order_bys) || is.list(order_bys))
